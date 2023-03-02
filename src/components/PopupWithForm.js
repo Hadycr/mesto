f4463f99b0b434055ae9960548b0f8ad61848fbc
Nadya <hadycr@gmail.com>
2023-03-02 22:17:17 +0400

    Исправлены ошибки 9 работа

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,13 +5,13 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._handleFormSumbit = handleFormSumbit;
     this._popupForm = this._popup.querySelector('.popup__form');
+    this._inputList = this._popupForm.querySelectorAll('.popup__input');
     this._submitButton = this._popupForm .querySelector('.popup__save');
     this._submitButtonText = this._submitButton.textContent;
   }
 
   _getInputValues() {
     this._formValues = {};
-    this._inputList = this._popupForm.querySelectorAll('.popup__input');
     this._inputList.forEach((input) => {
       this._formValues[input.name] = input.value;
     });
